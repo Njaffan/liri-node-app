ce8ebffa786fdc6646c995ddb51fb94da45f24c8
Nadir <Jaffan_Nadir@hotmail.com>
2017-10-29 16:07:43 -0700

    first commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+var twitterKeys = {
+    consumer_key: 'siFRWD4CknZCaSY0hc517XODn',
+    consumer_secret: 'Uheqxro0n6QmQqFv8qDBgN4Q3I6esjFadjykVtpy1txdqgFj8l',
+    access_token_key: 'icftDUg2l8StxuBrV5b2sNzkju9sjEFUkRhLXWbSo',
+    access_token_secret: 'GgXeWAjvGJ1YeWgx6QAv4D7kPh0XdctC2aLImZWkIdglI',
+  }
+  
+  module.exports = twitterKeys;
